6cf44e9de6417cc914c2fec4b56eec940deeff36
Denny Hollick <dennyhollick@gmail.com>
2017-08-02 19:05:18 -0700

    Created ew employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -4,8 +4,15 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
+require_relative '../lib/employee'
 
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Denny", last_name: "Hollick", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Dude", hourly_rate: 60)
+@store2.employees.create(first_name: "John", last_name: "Hello", hourly_rate: 60)
+
+puts Employee.count
