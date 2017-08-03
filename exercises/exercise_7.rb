1e318e72343e00edadec934059c5c2d3176ebc3f
Denny Hollick <dennyhollick@gmail.com>
2017-08-02 19:05:30 -0700

    Created a new store via user input

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,14 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name"
+
+store_name = gets.chomp
+
+create_store = Store.create name: "#{store_name}"
+
+if !create_store.valid?
+  pp create_store.errors.messages
+end
+
+
