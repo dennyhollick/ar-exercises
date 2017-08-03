52426b46b5e0b7ef44c3ac6bbc20825c8ffb1f7f
Denny Hollick <dennyhollick@gmail.com>
2017-08-02 19:04:17 -0700

    Found stores, updated one

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,12 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find_by(id: 1)
+puts @store1.inspect
+@store2 = Store.find_by(id: 2)
+puts @store2.inspect
+
+@store1.name = 'Burnaby-Central'
+@store1.save
+
+puts @store1.inspect
